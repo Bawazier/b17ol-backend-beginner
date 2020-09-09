be9c6abee883e6848dfb5c072821bdc5254c82a4
Bawazier <32612909+Bawazier@users.noreply.github.com>
2020-09-09 09:00:33 +0700

    create method post for '/data'

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 const express = require('express');
+const fetch = require('node-fetch');
+const { response } = require('express');
 const app = express();
+
+app.use(express.static('public'));
+app.use(express.json({ limit: '1mb' }));
+
+let data = {
+    id: 1,
+    name: 'saiful',
+    single: true 
+}
+
+const options = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+}
+
+fetch('http://localhost:8080/data', options)
 
 app.get('/home', (req, res) => {
     res.send('Welcome to my first backend!');
+});
+
+app.post('/data', (req, res) => {
+    console.log(req.body);
 });
   
 app.listen(8080, () =>{
